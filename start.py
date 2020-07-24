6452a086ea7ab5f7e18f3a101a8afecedae1dc25
aboro72 <andreas.borowczak@gmail.com>
2020-07-24 21:34:58 +0200

    Kommentar hinzugefügt.

--- start.py
+++ start.py
@@ -9,7 +9,9 @@ def debug():
 
 debug()
 
-
+''' Der eigentlich Codeblock ist mit absicht in ein Try gesetzt . Die Funktion debug() sollte eigentlich alle
+    fehler  aufzeichnen. Zumindest wenn alles richtig gemacht habe. Dort stehen auch die debug nachrichten von flask
+     wenn der debug=True parameter gesetzt ist in der funktion app.run().'''
 try:
     app = Flask(__name__)
 
