c9524c0553ae681ce673be852fc732a2ab21fd18
Mschumacher78 <Mschuma78@gmail.com>
2020-07-27 08:55:53 +0200

    Updated config.py
    Added configuration for production / testing and development
    
    Shell Commands:
    
    "export FLASK_ENV=development" - set to development environment
    "export FLASK_ENV=testing"     - set to testing environment
    "export FLASK_ENV=production"  - set to production environment
    
    The database login credentials are only if needed

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
 from flask_tinymce import Tinymce
 
+
+# -- Init FLASK & Config ---
 app = Flask(__name__)
+if app.config["ENV"] == "production":
+    app.config.from_object("config.ProductionConfig")
+elif app.config["ENV"] == "testing":
+    app.config.from_object("config.TestingConfig")
+else:
+    app.config.from_object("config.DevelopmentConfig")
 
 
 from app import routes
--- app/config.py
+++ app/config.py
@@ -1,2 +1,40 @@
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = "\x88|\x0c\x19q\xa5\\(\x8d9\x82!/\xfc\xa4\x8d\xdbe\xc7\x9a\x04s\x04\xe0"
+
+    # if we need the db credentials in the config
+    DB_NAME = "game-room-production"
+    DB_USER = "GameRoomAdminProduction"
+    DB_PASS = "LikeABoss"
+
+    SESSION_COOKIE_SECURE = True
+
+    # this is for uploads only
+    # UPLOAD_FOLDER = "/home/gameManager/flask-pos/app/files/upload"
+    # POSSIBLE_EXTENSIONS = set(['txt', 'jpg', 'png'])
 
 
+class ProductionConfig(Config):
+    pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+    # if we need the db credentials in the config
+    DB_NAME = "game-room-development"
+    DB_USER = "GameRoomAdminDevelopment"
+    DB_PASS = "LikeADev"
+
+    # this is for uploads only
+    # UPLOAD_FOLDER = "/home/dev/flask-pos/app/files/upload"
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+    # if we need the db credentials in the config
+    DB_NAME = "game-room-testing"
+    DB_USER = "GameRoomAdminTesting"
+    DB_PASS = "LikeATester"
