62b4dcaa4118dd7898cf251f7b01fc8142c025ba
Mschumacher78 <mschuma78@gmail.com>
2020-10-02 15:52:52 +0200

    Changed SameSite to Secure because it will be rejected in near future

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ class DevelopmentConfig(Config):
     # session lifetime in seconds
     PERMANENT_SESSION_LIFETIME = 600
 
-    SESSION_COOKIE_SAMESITE = 'None'
+    SESSION_COOKIE_SAMESITE = 'Strict'
     # database file
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
